4dd7f7e22e79d380b8f8f4f2f44267b936ab843a
CyrilDourthe <cyr.dourthe@gmail.com>
2017-11-14 21:09:42 +0100

    update

--- projet/cyril.py
+++ projet/cyril.py
@@ -1,8 +1,7 @@
 #-*- coding: utf-8 -*-
 
 def interface():
-    ligne = "+----"
-    colonne = "|    "
+    
     for i in range(21):
         if i % 2 ==0:
             print ligne
@@ -10,5 +9,30 @@ def interface():
             print colonne 
 
 
-"SALUT"
-interface()
+def create_grid(grid):
+    ligne = "+----+----+----+----+----+----+----+----+----+----+"
+    colonne = "|    |    |    |    |    |    |    |    |    |    |"
+    list=[1,2,3,4,5,6,7,8,9,10]
+    for i in range(21):
+        if i % 2 ==0:
+            print ligne
+            
+        else:
+            print "| ",grid.append(list)
+       
+
+#MAIN
+
+grid=[]
+
+x=5
+y=4
+
+create_grid(grid)
+
+grid[x][y]=0
+
+for i in range(10):
+    print grid[i]
+
+
