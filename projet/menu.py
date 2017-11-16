25359415608ba7efc2686dbeb6d18d2faf1a75a5
coddy33 <nanouwow@hotmail.fr>
2017-11-16 21:07:05 +0100

    update

--- projet/menu.py
+++ projet/menu.py
@@ -1,12 +1,32 @@
+#-*- coding: utf-8 -*-
 # Projet de programmation Python - Virus killer
 # JUNG Frédéric
 # DOURTHE Cyril
 # THOUVENIN Arthur
 
-#-*- coding: utf-8 -*-
+import os
 
-def grid(L):
+def create_grid(grid):
+    for i in range(10):
+        list=[1,2,3,4,5,6,7,8,9,10]
+        grid.append(list)
 
+def player(x,y):
+    movement = raw_input("où voulez-vous aller ?")
+    if movement == "z":
+        x = x+1
+    if movement == "s":
+        x = x-1
+    if movement == "d":
+        y = y+1
+    if movement == "q":
+        y = y-1
+    return x, y
+    #foreward
+    #backward
+    #left
+    #right
+    
 
 ######## MENU ########
 
@@ -16,6 +36,7 @@ def menu(lol):
     print "====================================== Virus Killer ======================================="
     print "\n"
     print "taper 1 pour commencer"
+    print "taper 2 pour vous déplacer"
     print "taper 0 pour quitter"
     print "\n"
     print "==========================================================================================="
@@ -25,7 +46,13 @@ def commandes(nb):
         menu(0)
         nb=raw_input() 
         if nb == "1":
-           
+            os.system("clear")
+            for i in range(10):
+                print grid[i]
+        if nb == "2":
+            player(x,y)
+            for i in range(10):
+                print grid[i]
         elif nb == "0":
             print "Bye"
             sys.exit()
@@ -36,9 +63,23 @@ def commandes(nb):
 ######## MAIN ########
 
 
+grid=[]
+
+x=5
+y=4
+
+create_grid(grid)
+
+grid[x][y]=0
+
+
+
+
 
 print "start ------>", 1 #faire la boucle avec la sortie
 print "stop  ------>", 0
 
 rep=input()
 commandes(rep)
+
+
