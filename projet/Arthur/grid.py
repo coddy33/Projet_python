f948df668cd1292b3ffcb7caa85b1b1a19dd6029
0AlphaZero0 <the-prodigy-moi@live.fr>
2017-11-17 11:43:26 +0100

    update

--- projet/Arthur/grid.py
+++ projet/Arthur/grid.py
@@ -27,17 +27,24 @@ def easy(grid):
     for i in range(len(listx)):
         grid[listx[i]][listy[i]]="W"
 
+def normal(grid):
+    listx=[2,8,7,3,7,4,0,2,0,4,9,1,6,1,6,3,0,8,2,6]
+    listy=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]
+    for i in range(len(listx)):
+        grid[listx[i]][listy[i]]="W"
 
-
-
-
+def hardcore(grid):
+    listx=[2,8,3,6,7,1,4,7,0,2,9,0,4,5,7,1,3,9,1,6,7,3,4,5,6,0,8,9,2,6]
+    listy=[0,0,1,1,1,2,2,2,3,3,3,4,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9]
+    for i in range(len(listx)):
+        grid[listx[i]][listy[i]]="W"
 
 #MAIN
 
 grid=[]
 
-x=5
-y=4
+x=4
+y=3
 
 create_grid(grid)
 
@@ -46,7 +53,7 @@ grid[x][y]=0
 #TEST GRID
 #for i in range(10):
     #print grid[i]
-easy(grid)
+hardcore(grid)
 print_grid(grid)
 
 
