a1c83b99f3480bb606b2e6075de6e670f13c50b0
coddy33 <nanouwow@hotmail.fr>
2017-11-25 20:00:46 +0100

    update

--- projet/menu.py
+++ projet/menu.py
@@ -32,31 +32,20 @@ def print_grid(grid):
         y=y+1
 
 
-"""
-#Localisation du joueur
-#vraiment nécessaire ? car initialisation coordonnées connus 
-def location(G,P):
-    x=0
-    y=0
-    for j in range(len(G)): #chercher la position du joueur
-        for i in G[j]: #mettre aussi dans une boucle for pour le G[j]
-            if i == 0:
-                y = G[j].index(0) # coordonées en y
-                x = j #coordonées en x
-                P.append(x)
-                P.append(y)
-"""
-
-
-def pop(nb,mol): #on defini le nombre de pop=nb ; puis quelle molecule doit pop=mol
+def pop(nb,mol,loc): #on defini le nombre de pop=nb ; puis quelle molecule doit pop=mol
     for i in range(nb):
         r=0    
         x=random.randint(0,9)
         listtmp=grid[x]
+        loc_virus=[]
         while r==0:
             y=random.randint(0,9)
             if listtmp[y]!=wall and listtmp[y] !=0 and listtmp[y]!=ATP and listtmp[y]!=virus:
                 grid[x][y]=mol
+                if mol == virus:
+                    loc_virus.append(x)
+                    loc_virus.append(y)
+                    loc.append(loc_virus)
                 break
             if listtmp[y]==wall or listtmp[y]==0 or listtmp[y]==ATP or listtmp[y]==virus:
                 continue
@@ -64,11 +53,37 @@ def pop(nb,mol): #on defini le nombre de pop=nb ; puis quelle molecule doit pop=
         
 #Movment Player
 
-def move(G,P,I):
+def move_player(G,P,I,P_V):
     movement = raw_input("où voulez-vous aller ?")
     nbr_case=input("de combien de cases voulez-vous vous déplacer ?") # erreur possible si input string
     x=P[0]
     y=P[1]
+    x,y = move(G,P,I,movement,nbr_case,x,y,P_V)
+    os.system("clear")
+    G[x][y]= perso
+    P[0]=x
+    P[1]=y
+
+def move_virus(G,P,I,P_V):
+    for i in range(len(P_V)):
+        nbr_case=random.randint(1,9) #nb de cases entre 1 et 9
+        listdirect=["z","q","s","d"]
+        pos=random.randint(0,3)
+        movement=listdirect[pos]
+        x=P_V[i][0]
+        y=P_V[i][1]
+        x,y = move(G,P,I,movement,nbr_case,x,y,P_V)
+        #del P_V[i]
+        G[x][y]= virus
+        P_V[i][0]=x
+        P_V[i][1]=y
+        
+def move(G,P,I,movement,nbr_case,x,y,P_V):
+    loc=0 # car pas de repop virus
+    if G[x][y] == virus:
+        pos = 1
+    else:
+        pos = 2
     G[x][y]=" " #remplacer l'ancienne position par une case vide
     x_step=0
     y_step=0
@@ -83,19 +98,21 @@ def move(G,P,I):
     for i in range(nbr_case):
         y=y+(y_step)
         x=x+(x_step)
-        if  test_grid(x,y,G) == False or G[x][y] == wall: # Pour ne pas sortir de la grille
+        if  test_grid(x,y,G) == False or G[x][y] == wall: # pas sortir de la grille ou un mur
             x,y = reculer(x,y,x_step,y_step)
             break
         elif G[x][y] == ATP: #ramassage des ATP
-            power_up(I) 
-            pop(nb=1,mol=ATP) 
-            G[x][y] = " " 
-    while G[x][y] == virus : #la dernière position ne peux pas être un virus
+            if pos == 2 :
+                power_up(I) 
+                pop(1,ATP,loc) 
+                G[x][y] = " " 
+    while G[x][y] == virus or G[x][y] == 0 or G[x][y] == ATP: #la dernière position ne peux pas être un virus
         x,y = reculer(x,y,x_step,y_step)
-    os.system("clear") # 0=le symbole qui matérialise le personnage dans la grille
-    G[x][y]= perso
-    P[0]=x
-    P[1]=y
+    return x,y
+    
+
+#def movement_player():
+
     
 def reculer(x,y,x_step,y_step):
     y=y-(y_step)
@@ -148,37 +165,72 @@ def power_up(I):
         if inventory[tmp][1]>8:
             inventory[tmp][1]=8
         i = i + 1
-            
-#def use_energy():
 
+def explode(G,a):
+    global nb_vir
+    x=position[0]
+    y=position[1]+1
+    size1=inventory[a][1]//2
+    size2=inventory[a][1]-size1
+    S=[size1,size2]
+    rand1=random.randint(0,1)
+    rand2=inventory[a][1]-S[rand1]
+    i=0
+    j=0
+    while i<S[rand1]:
+        if y>9:
+            break
+        if G[x][y]== virus :
+            G[x][y]=' '
+            nb_vir=nb_vir-1
+        #G[x][y]='B' #test peter les mur et size (cheat des devs)
+        y=y+1
+        i=i+1
+    y=position[1]-1
+    while j<rand2:
+        if y<0:
+            break
+        if G[x][y]=="\x1b[31;1mV\x1b[37;1m":
+            G[x][y]=' '
+            nb_vir=nb_vir-1
+        #G[x][y]='H' #test peter les murs et size (cheat des devs)
+        y=y-1
+        j=j+1
+    if nb_vir ==0:
+        os.system("clear")
+        print " __    __  _____   _   _        _          __  _   __   _  "
+        print " \ \  / / /  _  \ | | | |      | |        / / | | |  \ | | "
+        print "  \ \/ /  | | | | | | | |      | |  __   / /  | | |   \| | "
+        print "   \  /   | | | | | | | |      | | /  | / /   | | | |\   | "
+        print "   / /    | |_| | | |_| |      | |/   |/ /    | | | | \  | "
+        print "  /_/     \_____/ \_____/      |___/|___/     |_| |_|  \_| "
+        
+
+        
 def easy(grid):
     listx=[2,7,4,0,4,1,6,3,8,2]
     listy=[0,1,2,3,4,5,6,7,8,9]
     for i in range(len(listx)):
-        grid[listx[i]][listy[i]]=wall  
+        grid[listx[i]][listy[i]]=wall
 
 
 #END OF GAME
-#Essayer de mettre les deux dans une fonction
-
-def win(): #pas encore utilisé
-    print " __    __  _____   _   _        _          __  _   __   _  "
-    print " \ \  / / /  _  \ | | | |      | |        / / | | |  \ | | "
-    print "  \ \/ /  | | | | | | | |      | |  __   / /  | | |   \| | "
-    print "   \  /   | | | | | | | |      | | /  | / /   | | | |\   | "
-    print "   / /    | |_| | | |_| |      | |/   |/ /    | | | | \  | "
-    print "  /_/     \_____/ \_____/      |___/|___/     |_| |_|  \_| "
 
 def lose(I):
-    if len(I) == 0:
+    x=0
+    for i in I:
+        if i[1]==0:
+            x=x+1
+    if x==4:
+        os.system("clear")
         print "    __    __  _____   _   _        _       _____   _____   _____  "
         print "    \ \  / / /  _  \ | | | |      | |     /  _  \ /  ___/ | ____| "
         print "     \ \/ /  | | | | | | | |      | |     | | | | | |___  | |__   "
         print "      \  /   | | | | | | | |      | |     | | | | \___  \ |  __|  "
         print "      / /    | |_| | | |_| |      | |___  | |_| |  ___| | | |___  "
         print "     /_/     \_____/ \_____/      |_____| \_____/ /_____/ |_____| "
-    
 
+        
 def menu(lol):
     print "====================================== Virus Killer ======================================="
     print "\n"
@@ -191,15 +243,17 @@ def menu(lol):
 def commandes(nb):
     os.system("clear")
     easy(grid)
-    pop(nb=8,mol=ATP)
-    pop(nb=4,mol=virus)
+    pop(8,ATP,location_virus) 
+    pop(4,virus,location_virus) # 4 = number of Virus
     while nb != 0:
         print_grid(grid)
         menu(0)
         nb=raw_input() 
         if nb == "1":
             power_less(inventory)
-            move(grid,position,inventory)
+            move_player(grid,location_player,inventory,location_virus)
+            move_virus(grid,location_player,inventory,location_virus)
+            print inventory
             lose(inventory)
         elif nb == "2":
             os.system("clear")
@@ -220,10 +274,11 @@ inventory=[["Bombe nucléaire",8],["Grande bombe",6],["Moyenne bombe",4],["Petit
 
 grid=[]
 
-position=[]
+location_player=[] #position du joueur
+location_virus=[]
 
-position.append(0)
-position.append(0)
+location_player.append(0)
+location_player.append(0)
 
 wall="\x1b[30;1mW\x1b[37;1m"
 perso="\x1b[33;1m0\x1b[37;1m" 
@@ -234,7 +289,7 @@ create_grid(grid)
 
 #Initialisation  
 #le joueur est matérialisé par un 0 sur la grille
-grid[position[0]][position[1]]=0 #position initiale définie x=0 ;y=0
+grid[location_player[0]][location_player[1]]= perso #position initiale définie x=0 ;y=0
 
 os.system("clear")
 
@@ -253,6 +308,3 @@ print "[2] EXIT"
 rep=input()
 commandes(rep)
 
-#print l'inventaire dans le menu ?
-#régler les os. clear
-#faire la fonction pour perdre la partie
